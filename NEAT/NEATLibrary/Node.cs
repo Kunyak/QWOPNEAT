1010f4f0e076537ef65c21d5e22baa0100d98d81
Szilágyi Bence <szilube@gmail.com>
2018-10-05 03:21:19 +0200

    started work on phenotypes, not tested yet

---
+++ NEAT/NEATLibrary/Node.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NEATLibrary
+{
+    class Node
+    {
+        #region Node definition
+        public NodeType Type { get; }
+        //public int Id { get; }
+        public Dictionary<int, double> inputs;  //the int is the id of the node the input is coming from, the double is the connection weight
+        public Dictionary<int, double> outputs; //same here
+        public double LayerQuotient;
+
+        public Node(NodeType type, Dictionary<int, double> inConnections, Dictionary<int, double> outConnections, double layer)
+        {
+            Type = type;
+            //Id = id;
+            inputs = inConnections;
+            outputs = outConnections;
+            LayerQuotient = layer;
+        }
+
+        public Node(NodeType type, double layer)
+        {
+            Type = type;
+            //Id = id;
+            LayerQuotient = layer;
+        }
+        #endregion
+    }
+}
---
+++ NEAT/NEATLibrary/Phenotype.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NEATLibrary
+{
+    class Phenotype
+    {
+        #region Phenotype definition
+        public Dictionary<int, Node> Nodes; //the int is the id of the node
+
+        public Phenotype(Genome genome)
+        {
+            foreach(NodeGene nodeGene in genome.Nodes)
+            {
+                Nodes.Add(nodeGene.Id, Node(nodeGene.Type, nodeGene.LayerQuotient));
+            }
+
+            foreach(KeyValuePair<int, ConnectionGene> connection in genome.Connections)
+            {
+                Nodes[connection.Value.outNode].inputs.Add(connection.Value.inNode, connection.Value.Weight);
+                Nodes[connection.Value.inNode].outputs.Add(connection.Value.outNode, connection.Value.Weight);
+            }
+        }
+        #endregion
+        //should the nodes get ordered by layerquotient?
+    }
+}
